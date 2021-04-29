e0e0cddde1a5cb10292b0b4194d712e660582652
ADecastro <a.decastro1108@gmail.com>
2021-04-29 19:55:35 -0400

    updated api key

--- test_spoonacular/src/App.js
+++ test_spoonacular/src/App.js
@@ -4,8 +4,8 @@ import React, { useState } from "react";
 function App() {
   //const [searchData, setSearchData] = useState(null);
   //const [calories, setCalories] = useState(2000);
-  const apiKey = 'bb4c5730e058484aa07a4015606a1922';
-  //const apiKey = 'd8de6f34acff4ea89755be40ac8a5ca7';
+  //const apiKey = 'bb4c5730e058484aa07a4015606a1922';
+  const apiKey = 'd8de6f34acff4ea89755be40ac8a5ca7';
   function searchMeals() {
     fetch(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=pasta&number=2`
