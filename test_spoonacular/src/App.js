d80e476e0cd96a37f85bb35a93effa7c69d89b82
ADecastro <a.decastro1108@gmail.com>
2021-04-29 19:51:19 -0400

    Sample application to console.log search result objects and recipe information objects

---
+++ test_spoonacular/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, { useState } from "react";
+
+function App() {
+  //const [searchData, setSearchData] = useState(null);
+  //const [calories, setCalories] = useState(2000);
+  const apiKey = 'bb4c5730e058484aa07a4015606a1922';
+  //const apiKey = 'd8de6f34acff4ea89755be40ac8a5ca7';
+  function searchMeals() {
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=pasta&number=2`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        //setSearchData(data);
+        console.log(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function getRecipe(){
+    fetch(
+      `https://api.spoonacular.com/recipes/654883/information?apiKey=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+  
+  return (
+    <div>
+      <button onClick={searchMeals}>Search for pasta</button>
+      <button onClick={getRecipe}>Get Pasta and Vegetable Soup</button>
+    </div>
+  );
+}
+
+export default App;
